c9aeb7e72f60728e3572bd0c21e762dd97987b37
Yanuar Waskito <yg.waskito@gmail.com>
2016-01-19 15:09:00 +0700

    Hello World

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,37 @@
 var path = require('path');
 
 
-module.exports = {
-    entry: [
-      'webpack/hot/dev-server',
-      'webpack-dev-server/client?http://localhost:8080',
-      path.resolve(__dirname, 'app/main.js')
-    ],
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
+// module.exports = {
+//     entry: [
+//       'webpack/hot/dev-server',
+//       'webpack-dev-server/client?http://localhost:8080',
+//       path.resolve(__dirname, 'app/main.js')
+//     ],
+//     output: {
+//         path: path.resolve(__dirname, 'build'),
+//         filename: 'bundle.js',
+//     },
+// };
+
+var config = {
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    path.resolve(__dirname, 'app/main.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      query:{
+        presets:['react','es2015']
+      }
+    }]
+  }
 };
+
+module.exports = config;
